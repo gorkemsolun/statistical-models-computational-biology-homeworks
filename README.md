b7c6275bf64bdc0205ccbb67a042f140efa7a1c9
Bernhard Buss <54803043+bernhard-buss@users.noreply.github.com>
2026-02-21 18:05:07 +0100

    fix: 2) typo E -> F - add setup script - updarte README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # statistical-models-computational-biology
+
+## Setup
+Run setup.R to install packages to generate PDF from qmd.
---
+++ setup.R
@@ -0,0 +1,7 @@
+install.packages("BiocManager")
+BiocManager::install("graph")
+BiocManager::install("RBGL")
+BiocManager::install("BiDAG")
+BiocManager::install("Rgraphviz")
+
+library(BiDAG)
